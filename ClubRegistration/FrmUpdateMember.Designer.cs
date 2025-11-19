5a023f6f2a9ccda53ef479a72148304289257515
Xuee0904 <cedric2827@gmail.com>
2025-11-19 23:17:18 +0800

    Created UI for FrmUpdateMember

---
+++ ClubRegistration/FrmUpdateMember.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ClubRegistration
+{
+    partial class FrmUpdateMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.FemaleRadioBtn = new System.Windows.Forms.RadioButton();
+            this.MaleRadioBtn = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.AgeTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.MiddleNameTxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FirstNameTxt = new System.Windows.Forms.TextBox();
+            this.LastNameTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StudentIDTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(90)))), ((int)(((byte)(64)))));
+            this.UpdateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpdateButton.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.UpdateButton.Location = new System.Drawing.Point(526, 311);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(161, 55);
+            this.UpdateButton.TabIndex = 32;
+            this.UpdateButton.Text = "UPDATE";
+            this.UpdateButton.UseVisualStyleBackColor = false;
+            // 
+            // FemaleRadioBtn
+            // 
+            this.FemaleRadioBtn.AutoSize = true;
+            this.FemaleRadioBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FemaleRadioBtn.Location = new System.Drawing.Point(95, 265);
+            this.FemaleRadioBtn.Name = "FemaleRadioBtn";
+            this.FemaleRadioBtn.Size = new System.Drawing.Size(90, 29);
+            this.FemaleRadioBtn.TabIndex = 31;
+            this.FemaleRadioBtn.TabStop = true;
+            this.FemaleRadioBtn.Text = "Female";
+            this.FemaleRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // MaleRadioBtn
+            // 
+            this.MaleRadioBtn.AutoSize = true;
+            this.MaleRadioBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaleRadioBtn.Location = new System.Drawing.Point(17, 265);
+            this.MaleRadioBtn.Name = "MaleRadioBtn";
+            this.MaleRadioBtn.Size = new System.Drawing.Size(72, 29);
+            this.MaleRadioBtn.TabIndex = 30;
+            this.MaleRadioBtn.TabStop = true;
+            this.MaleRadioBtn.Text = "Male";
+            this.MaleRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 30);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Gender:";
+            // 
+            // AgeTxt
+            // 
+            this.AgeTxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgeTxt.Location = new System.Drawing.Point(363, 189);
+            this.AgeTxt.Name = "AgeTxt";
+            this.AgeTxt.Size = new System.Drawing.Size(324, 29);
+            this.AgeTxt.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(358, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 30);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Age:";
+            // 
+            // MiddleNameTxt
+            // 
+            this.MiddleNameTxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MiddleNameTxt.Location = new System.Drawing.Point(17, 189);
+            this.MiddleNameTxt.Name = "MiddleNameTxt";
+            this.MiddleNameTxt.Size = new System.Drawing.Size(324, 29);
+            this.MiddleNameTxt.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 30);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Middle name:";
+            // 
+            // FirstNameTxt
+            // 
+            this.FirstNameTxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNameTxt.Location = new System.Drawing.Point(17, 113);
+            this.FirstNameTxt.Name = "FirstNameTxt";
+            this.FirstNameTxt.Size = new System.Drawing.Size(324, 29);
+            this.FirstNameTxt.TabIndex = 24;
+            // 
+            // LastNameTxt
+            // 
+            this.LastNameTxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastNameTxt.Location = new System.Drawing.Point(363, 124);
+            this.LastNameTxt.Name = "LastNameTxt";
+            this.LastNameTxt.Size = new System.Drawing.Size(324, 29);
+            this.LastNameTxt.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 30);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "First name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(358, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 30);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Last name:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(363, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(324, 29);
+            this.comboBox1.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(358, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 30);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Program:";
+            // 
+            // StudentIDTxt
+            // 
+            this.StudentIDTxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentIDTxt.Location = new System.Drawing.Point(17, 43);
+            this.StudentIDTxt.Name = "StudentIDTxt";
+            this.StudentIDTxt.Size = new System.Drawing.Size(324, 29);
+            this.StudentIDTxt.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 30);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Student ID:";
+            // 
+            // FrmUpdateMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(708, 393);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.FemaleRadioBtn);
+            this.Controls.Add(this.MaleRadioBtn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.AgeTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.MiddleNameTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.FirstNameTxt);
+            this.Controls.Add(this.LastNameTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StudentIDTxt);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmUpdateMember";
+            this.Text = "FrmUpdateMember";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.RadioButton FemaleRadioBtn;
+        private System.Windows.Forms.RadioButton MaleRadioBtn;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox AgeTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox MiddleNameTxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox FirstNameTxt;
+        private System.Windows.Forms.TextBox LastNameTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox StudentIDTxt;
+        private System.Windows.Forms.Label label1;
+    }
+}
